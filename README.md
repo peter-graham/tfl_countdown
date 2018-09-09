4557168d1eb5e906d244d8afba146fe2a406cee5
Peter Graham <peter@petergraham.eu>
2018-09-09 19:02:35 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# tfl_countdown
+# TFL Countdown
+
+Super simple python2 class which pulls data from the TfL API
+
+*Requirements*
+Python 2
+
+*To run:*
+python2 tfl_countdown.py
+
---
+++ tfl_countdown/tfl_countdown.py
@@ -0,0 +1,64 @@
+import urllib, json
+from datetime import datetime
+
+
+class TfLArrivals:
+    URL = "https://api.tfl.gov.uk/StopPoint/{}/arrivals"
+
+    def __init__(self, stop):
+        self.stop_point = stop
+        self.trains = dict()
+
+    def arrivals_by_platform(self, platform=None):
+        """Returns a list of departure times, in seconds"""
+        result = []
+        for train in self.trains.values():
+            if platform is None or train["platformName"] == platform:
+                result.append(train["timeToStation"])
+        return sorted(result)
+
+    def arrivals_by_destination(self):
+        """Returns a dict of departure times, each value is a list of times in secs"""
+        result = dict()
+        for train in self.trains.values():
+            dest = train["destinationName"]
+
+            # populate destination dict
+            if dest not in result:
+                result[dest] = []
+
+            # populate arrival times
+            result[dest].append(train["timeToStation"])
+
+        # sort each list of departures once populated
+        for departures in result.values():
+            departures.sort()
+
+        return result
+
+    def tidy(self):
+        to_tidy = []
+        for id, train in self.trains.iteritems():
+            if self.convert_date(train["timeToLive"]) <= datetime.now():
+                to_tidy.append(id)
+
+        for id in to_tidy:
+            del self.trains[id]
+
+    def refresh(self):
+        response = urllib.urlopen(self.URL.format(self.stop_point))
+        data = json.loads(response.read())
+        for train in data:
+            self.trains[train["vehicleId"]] = train
+
+    @staticmethod
+    def convert_date(date):
+        return datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
+
+x = TfLArrivals("910GCSEAH")
+x.refresh()
+print x.arrivals_by_platform("Platform 1")
+print x.arrivals_by_destination()
+x.tidy()
+
+
